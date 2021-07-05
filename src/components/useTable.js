79b89be69bd93ac07b08e865a8aa7f2a8b7095d4
manos-fr <manoskakarakis@gmail.com>
2021-07-05 12:11:35 +0300

    css for table

--- src/components/useTable.js
+++ src/components/useTable.js
@@ -1,8 +1,35 @@
-import { Table, TableHead, TableRow, TableCell } from "@material-ui/core";
+import {
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  makeStyles,
+} from "@material-ui/core";
 import React from "react";
 
+const useStyles = makeStyles((theme) => ({
+  table: {
+    marginTop: theme.spacing(3),
+    "& thead th": {
+      fontWeight: "600",
+      color: theme.palette.primary.main,
+      backgroundColor: theme.palette.primary.light,
+    },
+    "& tbody td": {
+      fontWeight: "300",
+    },
+    "& tbody tr:hover": {
+      backgroundColor: "#fffbf2",
+      cursos: "pointer",
+    },
+  },
+}));
+
 export default function useTable(records, headCells) {
-  const TblContainer = (props) => <Table>{props.children}</Table>;
+  const classes = useStyles();
+  const TblContainer = (props) => (
+    <Table className={classes.table}>{props.children}</Table>
+  );
 
   const TblHead = (props) => {
     return (
