2f0b8b0f49c6ddb7254073b7d0ba73349112dcf0
manos-fr <manoskakarakis@gmail.com>
2021-07-05 13:08:21 +0300

    sorting with materialui completed

--- src/components/useTable.js
+++ src/components/useTable.js
@@ -7,8 +7,6 @@ import {
   TableCell,
   makeStyles,
 } from "@material-ui/core";
-import { SettingsBackupRestoreRounded } from "@material-ui/icons";
-import { parseISOWithOptions } from "date-fns/fp";
 import React, { useState } from "react";
 
 const useStyles = makeStyles((theme) => ({
@@ -52,7 +50,10 @@ export default function useTable(records, headCells) {
       <TableHead>
         <TableRow>
           {headCells.map((headCell) => (
-            <TableCell key={headCell.id}>
+            <TableCell
+              key={headCell.id}
+              sortDirection={orderBy === headCell.id ? order : false}
+            >
               <TableSortLabel
                 active={orderBy === headCell.id}
                 direction={orderBy === headCell.id ? order : "asc"}
@@ -90,8 +91,37 @@ export default function useTable(records, headCells) {
     />
   );
 
+  function stableSort(array, comparator) {
+    const stabilizedThis = array.map((el, index) => [el, index]);
+    stabilizedThis.sort((a, b) => {
+      const order = comparator(a[0], b[0]);
+      if (order !== 0) return order;
+      return a[1] - b[1];
+    });
+    return stabilizedThis.map((el) => el[0]);
+  }
+
+  function getComparator(order, orderBy) {
+    return order === "desc"
+      ? (a, b) => descendingComparator(a, b, orderBy)
+      : (a, b) => -descendingComparator(a, b, orderBy);
+  }
+
+  function descendingComparator(a, b, orderBy) {
+    if (b[orderBy] < a[orderBy]) {
+      return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+      return 1;
+    }
+    return 0;
+  }
+
   const recordsAfterPagingAndSorting = () => {
-    return records.slice(page * rowsPerPage, (page + 1) * rowsPerPage);
+    return stableSort(records, getComparator(order, orderBy)).slice(
+      page * rowsPerPage,
+      (page + 1) * rowsPerPage
+    );
   };
 
   return {
