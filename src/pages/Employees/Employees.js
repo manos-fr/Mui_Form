c86c25470ca6e2fa7a77aad26de2e372008e8ff6
manos-fr <manoskakarakis@gmail.com>
2021-07-05 12:04:42 +0300

    added table with employees and header

--- src/pages/Employees/Employees.js
+++ src/pages/Employees/Employees.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import EmployeeForm from "./EmployeeForm";
 import PageHeader from "../../components/PageHeader";
 import PeopleIcon from "@material-ui/icons/People";
-import { Paper, makeStyles } from "@material-ui/core";
+import { Paper, makeStyles, TableBody } from "@material-ui/core";
+import useTable from "../../components/useTable";
+import * as employeeService from "../../services/employeeService";
+import { TableRow, TableCell } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   pageContent: {
@@ -11,8 +14,19 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const headCells = [
+  { id: "fullName", label: "Employee Name" },
+  { id: "email", label: "Email Address" },
+  { id: "mobile", label: "Mobile Number" },
+  { id: "department", label: "Department" },
+];
+
 export default function Employees() {
   const classes = useStyles();
+  const [records, setRecords] = useState(employeeService.getAllEmployees);
+
+  const { TblContainer, TblHead } = useTable(records, headCells);
+
   return (
     <>
       <PageHeader
@@ -21,7 +35,20 @@ export default function Employees() {
         icon={<PeopleIcon fontSize="large" />}
       />
       <Paper className={classes.pageContent}>
-        <EmployeeForm />
+        {/* <EmployeeForm /> */}
+        <TblContainer>
+          <TblHead />
+          <TableBody>
+            {records.map((item) => (
+              <TableRow key={item.id}>
+                <TableCell>{item.fullName}</TableCell>
+                <TableCell>{item.email}</TableCell>
+                <TableCell>{item.mobile}</TableCell>
+                <TableCell>{item.department}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </TblContainer>
       </Paper>
     </>
   );
